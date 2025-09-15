65d8a3b99533d7b5f36eb8ae1b70b9c05b6b341d
Federico Nery <fedegnery@gmail.com>
2025-09-15 14:28:15 -0300

    Added browser language logic

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,11 +1,17 @@
 import { getRequestConfig } from 'next-intl/server';
-import { cookies } from 'next/headers';
+import { cookies, headers } from 'next/headers';
 
 export default getRequestConfig(async () => {
+/*   const browserHeaders = headers();
+  const acceptLanguage = await browserHeaders.get('accept-language');
+  // Extrae el primer idioma y elimina la región si existe
+  const browserLocale = acceptLanguage?.split(',')[0]?.split('-')[0] || 'en'; */
+
   const cookieLocale = await cookies()
   const cookieValue = cookieLocale.get('locale')?.value;
-  const browserLocale = cookieValue || 'en'; // fallback
-    console.log("Locale detectada: " + browserLocale);
+  const browserLocale = cookieValue || 'en'; 
+  
+  console.log("Locale detectada: " + browserLocale);
   return {
     locale: browserLocale,
     messages: (await import(`../../messages/${browserLocale}.json`)).default
